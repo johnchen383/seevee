66c5a26908887ecd2862ea34343f2d5ce26a733d
John Chen <john.bm.chen@gmail.com>
2021-04-13 17:31:36 +1200

    adding modal and first commit for rick roll ;)

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,9 @@
 /*please have all your inserts in the following format. custom.css for general page and main js changes*/
+
+
+const rickButton = document.querySelector('rick');
+rickButton.addEventListener('click', rickRoll)
+
+function rickRoll(){
+    const aTags = document.querySelectorAll(a)
+}
