74bf97b143d050ac16c8011c946a9fca7ba6800d
John Chen <john.bm.chen@gmail.com>
2021-04-16 21:22:57 +1200

    got the logic working/ wanting to fix the styling after

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,14 @@
 /*please have all your inserts in the following format. custom.css for general page and main js changes*/
 
+// javascript for videomodal selection
+const projectItems = document.querySelectorAll('.project-item');
 
-const rickButton = document.querySelector('rick');
-rickButton.addEventListener('click', rickRoll)
-
-function rickRoll(){
-    const aTags = document.querySelectorAll(a)
-}
+projectItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+        // console.log('clicked');
+        var url = item.title;
+        const vidModal = document.querySelector('#vidModalLink');
+        vidModal.src = url;
+        // vidModal.div.div.div.iframe.src = url;
+    });
+});
