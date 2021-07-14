ca5d207f702625509e7a7b46b0b5761bbaeeabae
John Chen <john.bm.chen@gmail.com>
2021-07-14 19:30:04 +1200

    started chinese translation ;)

--- john/john.js
+++ john/john.js
@@ -2,7 +2,6 @@ const darkModeBtn = document.querySelector('#darkBtn')
 var onDark = false
 
 darkModeBtn.addEventListener('click', function () {
-    console.log('ckicked!')
     if (!onDark){
         document.documentElement.style.setProperty('--primary', 'rgb(21, 231, 21)');
         document.documentElement.style.setProperty('--primarybar', '#333');
@@ -16,3 +15,17 @@ darkModeBtn.addEventListener('click', function () {
     }
     onDark = !onDark;
 });
+
+const langBtn = document.querySelector('#langBtn')
+var onChinese = false
+
+langBtn.addEventListener('click', function (){
+    console.log('ckicked!')
+    if (!onChinese){
+        document.querySelector('#about div div.subheading').innerHTML = "软件工程师 · <a href=\"tel:+64021008587480\">(021) 085-87480</a> · <a href='mailto:john.bm.chen@gmail.com'>john.bm.chen@gmail.com</a>";
+    } else {
+        document.querySelector('#about div div.subheading').innerHTML = "Software Engineer BE(Hons) · <a href=\"tel:+64021008587480\">(021) 085-87480</a> · <a href='mailto:john.bm.chen@gmail.com'>john.bm.chen@gmail.com</a>";
+    }
+    onChinese = !onChinese;
+});
+
